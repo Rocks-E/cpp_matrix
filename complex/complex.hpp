b7d1ebc6e43776bd1720a6208673583d6df39ece
Rocks-E <RoxySRobbins@gmail.com>
2025-03-16 12:11:02 -0700

    More declarations - moving on to implementation

--- complex/complex.hpp
+++ complex/complex.hpp
@@ -1,3 +1,4 @@
+#include <cmath> // For trig functions
 
 namespace rsr {
 	
@@ -20,6 +21,15 @@ namespace rsr {
 		double imag() const;
 		double &imag();
 		
+		/* "Hypotenuse" - sometimes called absolute value for complex numbers, using hyp here to allow us to use abs for making both parts positive */
+		double hyp() const;
+		/* "Absolute value" - used here like real absolute values */
+		complex abs() const;
+		
+		/* Simple copy */
+		void operator=(const complex &z);
+		
+		/* Basic arithmetic operations */
 		void operator+=(const complex &z);
 		void operator-=(const complex &z);
 		void operator*=(const complex &z);
@@ -28,6 +38,16 @@ namespace rsr {
 		complex operator-(const complex &z) const;
 		complex operator*(const complex &z) const;
 		complex operator/(const complex &z) const;
+		
+		/* Powers (oh goodness) */
+		void operator^=(const complex &z);
+		complex operator^(const complex &z) const;
+		
+		/* Complex conjugate */
+		complex operator~() const;
+		/* Negation */
+		complex operator-() const;
+		
 		
 	}
 	
@@ -38,3 +58,4 @@ complex operator+(const double a, const complex &z);
 complex operator-(const double a, const complex &z);
 complex operator*(const double a, const complex &z);
 complex operator/(const double a, const complex &z);
+complex operator^(const double a, const complex &z);
